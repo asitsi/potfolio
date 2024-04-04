8c089e43b299c50032f7d39e0e5aba6abe247744
akki74 <akki36712@gmail.com>
2024-04-04 10:49:40 +0530

    SEO Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home/Home";
@@ -5,16 +6,25 @@ import About from "./components/Aboutus/About";
 import Projects from "./components/Projects/Project";
 import Cartificate from "./components/Cartificats/Cartifates";
 import Contact from "./components/Contact/Contact";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Cartificate />
-      <Projects />
-      <Contact />
-    </div>
+    <React.Fragment>
+      <Helmet>
+        <title>Web Development & Mobile Developer Portfolio | Asit singh</title>
+        <meta property="og:title" content="Potfolio: Web Development & Mobile Developer Portfolio | Projects" />
+        <meta property="og:description" content="Explore our collection of hash and RGB colors. Simply click on any color to copy its code instantly. Discover the perfect color for your projects with ease. Start creating stunning designs now! | RGB color codes, HTML color picker, Color conversion" />
+        <meta property="og:image" content="potfolio.png" />
+      </Helmet>
+      <div className="App">
+        <Home />
+        <About />
+        <Cartificate />
+        <Projects />
+        <Contact />
+      </div>
+    </React.Fragment>
   );
 }
 
